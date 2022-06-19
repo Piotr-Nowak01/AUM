e7372fa0b20a208388fe276882cc60c54e633664
Piotr Nowak <62243821+Piotr-Nowak01@users.noreply.github.com>
2022-06-19 09:26:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import time
-
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.model_selection import learning_curve, ShuffleSplit
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -122,13 +124,101 @@ def Usage_Combined(file):
     print("Wyniki: " + str(Y_predicted))
 def Visualisation(method):
     print("WIP")
+    filename = "model_" + method + ".sav"
+    if not exists(filename):  # checking if model exists
+        print("Nie ma takiego modelu. Próbuję taki stworzyć.")
+        test = Training(method, "Tweety.csv")
+        if test:
+            print("Udało się stworzyć model.")
+        else:
+            return
+    estimator = pickle.load(open(filename, 'rb'))
+    title = "Krzywa uczenia modelu "+str(method)
+    X, y = load_digits(return_X_y=True)
+    fig, axes = plt.subplots(3, 1, figsize=(10, 15))
+    cv = ShuffleSplit(n_splits=50, test_size=0.2, random_state=0)
+    plot_learning_curve(estimator, title, X, y, axes=axes[:], ylim=(0.7, 1.01), cv=cv, n_jobs=4)
+    plt.show()
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None, n_jobs=None,train_sizes=np.linspace(0.1, 1.0, 5)): #this function is taken from scikit-learn library documentaction
+    if axes is None:
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, return_times=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(
+        train_sizes,
+        train_scores_mean - train_scores_std,
+        train_scores_mean + train_scores_std,
+        alpha=0.1,
+        color="r",
+    )
+    axes[0].fill_between(
+        train_sizes,
+        test_scores_mean - test_scores_std,
+        test_scores_mean + test_scores_std,
+        alpha=0.1,
+        color="g",
+    )
+    axes[0].plot(
+        train_sizes, train_scores_mean, "o-", color="r", label="Training score"
+    )
+    axes[0].plot(
+        train_sizes, test_scores_mean, "o-", color="g", label="Cross-validation score"
+    )
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, "o-")
+    axes[1].fill_between(
+        train_sizes,
+        fit_times_mean - fit_times_std,
+        fit_times_mean + fit_times_std,
+        alpha=0.1,
+    )
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    fit_time_argsort = fit_times_mean.argsort()
+    fit_time_sorted = fit_times_mean[fit_time_argsort]
+    test_scores_mean_sorted = test_scores_mean[fit_time_argsort]
+    test_scores_std_sorted = test_scores_std[fit_time_argsort]
+    axes[2].grid()
+    axes[2].plot(fit_time_sorted, test_scores_mean_sorted, "o-")
+    axes[2].fill_between(
+        fit_time_sorted,
+        test_scores_mean_sorted - test_scores_std_sorted,
+        test_scores_mean_sorted + test_scores_std_sorted,
+        alpha=0.1,
+    )
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt
 def Help():
     print("Funkcja \"Training\" pozwala na wytrenowanie modelu na podstawie pliku z danymi. Należy pamiętać, że w każdym wierszu powinien znaleźć się tekst oraz klasyfikacja tekstu. 1 dla tekstów nadających się do publikacji oraz 0, dla tekstów nienadających się do publikacji.")
     print("Funkcja \"Statistics\" pozwala na sprawdzenie współczynników jakości dla danego modelu algorytmu uczenia maszynowego stworzonego na podstawie danych uczących.")
     print("Funkcja \"Usage\" pozwala na wykorzystanie gotowego modelu do predykcji etykiet dla podanych danych. W przypadku, gdyby wcześniej nie stworzono modelu, to program najpierw stworzy model dla danego algorytmu na podstawie wcześniej przygotowanych danych uczących a następnie wykorzysta stworzony model do predykcji podanych danych.")
     print("Funkcja \"Help\" wyświetla to okno z wyjaśnieniem działania poszczególnych funkcji programu.")
     print("Funkcja \"UsageCombined\" działa analogicznie do funkcji \"Usage\", lecz zamiast pojedyńczego algorytmu wyciąga średnią ważoną z predykcji wszystkich 3 modeli. Wagi zostały przydzielone zgodnie z dokładnością algorytmów.")
-    print("Funkcja \"Visualisation\" pozwala na wizualizację krzywej uczenia dla danego algorytmu uczenia maszynowego.")
+    print("Funkcja \"Visualisation\" pozwala na wizualizację krzywej uczenia dla danego algorytmu uczenia maszynowego. UWAGA: nie działa dla metody KNN.")
     x = input("Wciśnij dowolny przycisk, aby wrócić do menu głównego.")
     return
 def Menu():
